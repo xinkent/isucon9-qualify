6fc82ddf853e7649759ddaf479ff5e0c15587752
xinkent <xinkent1994@gmail.com>
2021-08-13 23:55:12 +0000

    add scripts

---
+++ scripts/bench.sh
@@ -0,0 +1,4 @@
+# sudo rm /var/log/nginx/access.log
+# sudo rm /var/log/mysql/mysql-slow.log
+
+cd ~/isucari; ./bin/benchmarker -target-url http://127.0.0.1:80
---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+sudo cp -r ~/git/mysql /etc/
+sudo cp -r ~/git/nginx /etc/
+
+sudo systemctl restart nginx
+sudo systemctl restart mysql
+

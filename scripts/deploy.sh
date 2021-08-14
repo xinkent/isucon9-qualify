bcb1d61f400bbc4175da4f919221732732af3958
xinkent <xinkent1994@gmail.com>
2021-08-14 10:27:05 +0000

    update deplooy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
+sudo systemctl stop isucari.golang
+
 sudo cp -r ~/git/mysql /etc/
 sudo cp -r ~/git/nginx /etc/
 sudo cp -r ~/git/webapp/go /home/isucon/isucari/webapp/
 sudo cp -r ~/git/webapp/sql /home/isucon/isucari/webapp/
 
-sudo systemctl stop isucari.golang
-cd /home/isucon/isucari/webapp/go; rm /home/isucon/isucari/webapp/go/isucari; make;
+cd /home/isucon/isucari/webapp/go; rm -f /home/isucon/isucari/webapp/go/isucari; make;
 sudo systemctl restart isucari.golang
 

f45c61334f2fb2b676a0d784d775eae447e5035e
xinkent <xinkent1994@gmail.com>
2021-08-14 00:54:15 +0000

    add sleep for bench.sh

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,4 +1,14 @@
-# sudo rm /var/log/nginx/access.log
-# sudo rm /var/log/mysql/mysql-slow.log
+# アクセスログ削除
+sudo rm /var/log/nginx/access.log
+sudo rm /var/log/mysql/mysql-slow.log
 
+# サービス再起動
+sudo systemctl restart nginx
+sudo systemctl restart mysql
+
+sleep 10
+
+# sudo systemctl status mysql
+
+# ベンチ実行
 cd ~/isucari; ./bin/benchmarker -target-url http://127.0.0.1:80
